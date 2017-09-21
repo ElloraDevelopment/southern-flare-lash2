b4235bbbb8635693545038fd30aebc4509dd1703
Sarah Baldwin <sarahb1245@gmail.com>
2017-09-21 17:27:04 -0600

    change to product form

--- src/cms/products-cms/cms-add-products-components/products-form.js
+++ src/cms/products-cms/cms-add-products-components/products-form.js
@@ -10,7 +10,7 @@ class ProductsForm extends React.Component {
         <input type="number" placeholder="price" value={this.props.input.price} onChange={(event) => {this.props.handleChange("price", event)}} />
         <label>Check if Active</label>
         <input type="checkbox" checked={this.props.input.active} onChange={(event) => {this.props.handleChange("active", event)}} />
-        <button className="add-item" onClick={() => {this.props.handleClick(this.props.input); this.props.clearInputs();}}>Add Item</button>
+        <button className="add-item" onClick={() => {this.props.handleClick(this.props.input); this.props.clearInputs();}}>Add Product</button>
         {/* <button className="edit-item">Edit Item</button>
         <button className="remove-item">Remove Item</button> */}
       </div>
