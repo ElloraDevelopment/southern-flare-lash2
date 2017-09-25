0f643cede83f49e21b5f04c058b5df49be655441
Sarah Baldwin <sarahb1245@gmail.com>
2017-09-24 19:30:14 -0600

    product page styled

--- src/ecommerce/product-page/components/product-page-component.js
+++ src/ecommerce/product-page/components/product-page-component.js
@@ -8,14 +8,18 @@ class ProductPageComponent extends React.Component {
           <div className="product-pic" style={{backgroundImage: `url(${this.props.product.pic})`}}></div>
         </div>
         <div className="product-info">
-          <h3>{this.props.product.title}</h3>
-          <h4>{this.props.product.desc}</h4>
-          <h4>${this.props.product.price}</h4>
-          <h4>enter quantity and add to cart:</h4>
-          <input type="number" placeholder="quantity" value={this.props.input.quantity} onChange={(event) => {this.props.handleChange("quantity", event)}}/>
-          <button onClick={() =>
-            {this.props.updateCart(this.props.product._id, this.props.product.pic, this.props.product.title, this.props.product.desc, this.props.product.price, this.props.product.quantity)}}
-            >Add to Basket</button>
+          <h3 className="product-title">{this.props.product.title}</h3>
+          <h4 className="product-desc">{this.props.product.desc}</h4>
+          <div className="price-cart">
+            <h4 className="product-price">${this.props.product.price}</h4>
+            <button className="add-button" onClick={() =>
+              {this.props.updateCart(this.props.product._id, this.props.product.pic, this.props.product.title, this.props.product.desc, this.props.product.price, this.props.product.quantity)}}
+              >Add to Cart</button>
+          </div>
+          {/* <div className="quantity-cart"> */}
+            {/* <h4>enter quantity and add to cart:</h4> */}
+            {/* <input className="add-quantity" type="number" placeholder="quantity" value={this.props.input.quantity} onChange={(event) => {this.props.handleChange("quantity", event)}}/> */}
+          {/* </div> */}
         </div>
       </div>
     )
