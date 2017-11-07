0d7d20801deb176312b914deacf273f08aaa873d
Kacie Jacobson <kacieaken@gmail.com>
2017-11-06 20:05:52 -0700

    Alyssa number changes

--- src/contact.js
+++ src/contact.js
@@ -16,11 +16,11 @@ class Contact extends React.Component {
                 <div className="row">
                     <div className="col-md-6 col-sm-6 col-xs-12 contact-info">
                         <div className="link-wrapper">
-                            <a className="contact-links" href="tel:336-212-0188"><strong>Phone</strong> | (336)-212-0188</a>
+                            <a className="contact-links" target="_blank" href="tel:336-212-0188"><strong>Phone</strong> | (307)-277-1828</a>
                     
-                            <a className="contact-links" href="https://www.google.com/maps/place/316+W+Midwest+Ave,+Casper,+WY+82601/@42.8462751,-106.3306873,17z/data=!3m1!4b1!4m5!3m4!1s0x8760bca14c5b7b2b:0xa8acf35ed02cdaf0!8m2!3d42.8462751!4d-106.3284986"><strong>Address</strong> | 316 W Midwest Ave Casper, WY 82601</a>
+                            <a className="contact-links" target="_blank" href="https://www.google.com/maps/place/314+W+Midwest+Ave,+Casper,+WY+82601/@42.8462751,-106.3306873,17z/data=!3m1!4b1!4m5!3m4!1s0x8760bca14c5b7b2b:0xa8acf35ed02cdaf0!8m2!3d42.8462751!4d-106.3284986"><strong>Address</strong> | 314 W Midwest Ave Casper, WY 82601</a>
                     
-                            <a className="contact-links" href="mailto:southernFlareLash@hotmail.com"><strong>Email</strong> | southernflarelash@hotmail.com</a>
+                            <a className="contact-links" target="_blank" href="mailto:southernFlareLash@hotmail.com"><strong>Email</strong> | southernflarelash@hotmail.com</a>
                             <div className="contact-icons-wrapper text-center">
                                 <a className="contact-icons" href="tel:+1-336-212-0188" >
                                     <FontAwesome name="phone" size="2x"/>
@@ -40,7 +40,7 @@ class Contact extends React.Component {
                             </div>
 
                             <center>    
-                                <a activeHref href="https://www.schedulicity.com/scheduling/SFLPW8" className="custom-nav-btn">
+                                <a activeHref target="_blank" href="https://www.schedulicity.com/scheduling/SFLPW8" className="custom-nav-btn">
                                     <span className="btn nav-schedule book-now">Book Now</span>
                                 </a>
                             </center>
--- src/footer.js
+++ src/footer.js
@@ -7,7 +7,7 @@ class Footer extends React.Component {
         return (
             <div className="footer">
                 <div className="icon-wrapper">
-                <a href="tel:+1-336-212-0188" className="icons">
+                <a target="_blank" href="tel:+1-307-277-1828" className="icons">
                         <FontAwesome name="phone" size="2x"/>
                     </a>
 
@@ -33,9 +33,9 @@ class Footer extends React.Component {
                         <div className="footer-links footer-inline">Login</div>
                     </Link>
 
-                    <div><a className="footer-links" href="tel:336-212-0188">336-212-0188</a></div>
+                    <div><a className="footer-links" target="_blank" href="tel:307-277-1828">307-277-1828</a></div>
 
-                    <div><a className="footer-links" href="https://github.com/ElloraDevelopment">Created by Ellora Dev<FontAwesome className="git-icon" name="github" size="1x"/></a></div>
+                    <div><a className="footer-links" target="_blank" href="https://github.com/ElloraDevelopment">Created by Ellora Dev<FontAwesome className="git-icon" name="github" size="1x"/></a></div>
                 
             </div>
         )
