edf334a441373300307629e1c0e8a43d4e7d195d
Kacie Jacobson <kacieaken@gmail.com>
2017-11-14 12:38:05 -0700

    icons back to png & BrowserRouter fix

--- src/landing/services.js
+++ src/landing/services.js
@@ -10,9 +10,8 @@ class Services extends Component {
 
                 <div className="service-col">
                     <Link to="/lashes">
-                        <img className="home-icon img-responsive" src="images/sfl-pics/home-icons/lash-icon.jpg" alt="lash-icon"/>
+                        <img className="home-icon img-responsive" src="images/sfl-pics/home-icons/lash-icon.png" alt="lash-icon"/>
                         <p className="service-text lash-title">Lashes</p>
-                        {/*<div className="home-service mega" style={{backgroundImage: 'url("images/sfl-pics/mega-volume/mega1.jpg")'}}></div>   */}
                     </Link>
                 </div>
 
@@ -20,10 +19,9 @@ class Services extends Component {
                 
                 <div className="service-col">
                     <Link to="/perm_makeup">
-                        <img className="home-icon img-responsive" src="images/sfl-pics/home-icons/lips-icon.jpg" alt="lash-icon"/>
+                        <img className="home-icon img-responsive" src="images/sfl-pics/home-icons/lips-icon.png" alt="lash-icon"/>
                         <p className="service-text makeup-desktop">Permanent Makeup</p>
                         <p className="service-text makeup-mobile">Perm. Makeup</p>
-                        {/*<div className="home-service perm-img" style={{backgroundImage: 'url("https://thumb1.shutterstock.com/display_pic_with_logo/2570710/551368453/stock-photo-permanent-makeup-permanent-tattooing-of-eyebrows-cosmetologist-applying-permanent-make-up-on-551368453.jpg")'}}></div>   */}
                     </Link>
                 </div>
 
@@ -31,9 +29,8 @@ class Services extends Component {
                 
                 <div className="service-col">
                     <Link to="/waxing">
-                        <img className="home-icon img-responsive" src="images/sfl-pics/home-icons/wax-icon.jpg" alt="lash-icon"/>
+                        <img className="home-icon img-responsive" src="images/sfl-pics/home-icons/wax-icon.png" alt="lash-icon"/>
                         <p className="service-text">Waxing</p>
-                        {/*<div className="home-service wax-img" style={{backgroundImage: 'url("images/sfl-pics/home/home-waxing.jpg")'}}></div>   */}
                     </Link>
                 </div>
 
@@ -41,9 +38,8 @@ class Services extends Component {
 
                 <div className="service-col text-center">
                     <Link to="/facials">
-                        <img className="home-icon img-responsive " src="images/sfl-pics/home-icons/face-icon1.jpg" alt="lash-icon"/>
+                        <img className="home-icon img-responsive " src="images/sfl-pics/home-icons/face-icon1.png" alt="lash-icon"/>
                         <p className="service-text ">Facials</p>
-                        {/*<div className="home-service face-img" style={{backgroundImage: 'url("images/sfl-pics/home/home-facial.jpg")'}}></div>   */}
                     </Link>
                 </div>
 
